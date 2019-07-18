1d2b94a9373fca540a221f39de09f7a9e8cfe0c7
nico12450 <nicolas.gamel12450@gmail.com>
2019-07-18 16:02:05 +0200

    factorisation de certaines parties du code

--- serveur.js
+++ serveur.js
@@ -7,11 +7,18 @@ const directoryPath = path.join(__dirname, '');
 
 const homedir = require('os').homedir();
 
+var adresseFichier = homedir;
+
 //passsing directoryPath and callback function
 
-var listeRepertoire = [];
+//var listeRepertoire = [];
 
-fs.readdir(homedir,{withFileTypes:true}, function (err, files) {
+//lireRepertoire(adresseFichier);
+
+/*
+
+fs.readdir(adresseFichier,{withFileTypes:true}, function (err, files) {
+
     //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
@@ -28,6 +35,8 @@ fs.readdir(homedir,{withFileTypes:true}, function (err, files) {
     listeRepertoire = triDossier(listeRepertoire);
 
 });
+
+*/
 
 
 function triDossier(listeFichiers){
@@ -52,44 +61,100 @@ function triDossier(listeFichiers){
 
 }
 
+function lireRepertoire(chemin,callback){
+
+    //console.log(chemin);
+
+    var lRep = [];
+
+    fs.readdir(chemin,{withFileTypes:true}, function (err, files) {
+
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        //listing all files using forEach
+        files.forEach(function (file) {
+
+            //console.log(file);
+            lRep.push(file);
+
+        });
+
+        //console.log(listeRepertoire);
+        lRep = triDossier(lRep);
+        //console.log(listeRepertoire);
+        callback(lRep);
+
+    });
+
+}
+
 
 
 var http = require('http');
 
 const PORT=8080; 
 
+var fichierHTML;
+
 fs.readFile('./Main.html', function (err, html) {
 
-    if (err) throw err;    
+    if (err) throw err;
 
-    http.createServer(function(request, response) { 
+    fichierHTML = html;
 
-	    //response.writeHeader(200, {'Content-Type': 'application/json'});
-   		//response.write(JSON.stringify(listeRepertoire));
 
-		response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
+});
+
+
+http.createServer(function(request, response) { 
+
+    //response.writeHeader(200, {'Content-Type': 'application/json'});
+    //response.write(JSON.stringify(listeRepertoire));
+
+    var adresse = request.url;
+
+    console.log(adresse);
+
+    //if(adresse == "/favicon.ico"){adresse = "/";}
+
+    adresseFichier = path.join(adresseFichier,adresse);
+
+    lireRepertoire(adresseFichier, function(listeRepertoire){
+
+        //console.log(listeRepertoire);
+
+        //console.log(adresseFichier);
+
+        //console.log(listeRepertoire);
+
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(fichierHTML);
+
 
         for(var i = 0; i<listeRepertoire.length; i++){
 
-        	var element = listeRepertoire[i];
+            var element = listeRepertoire[i];
 
-        	if (element.isDirectory()) {
+            if (element.isDirectory()) {
 
-        		response.write("$('#listeFichiers').append(\"<div class='col-md-2 col-sm-3 pt-4 pb-4 border hoverable' id='dossier'> " + "<i class='fas fa-folder-open'></i>" + " " + element.name + " </div>\");");
-        	}
+                response.write("$('#listeFichiers').append(\"<div class='col-md-2 col-sm-3 pt-4 pb-4 border hoverable' id='dossier' onclick='window.location=`" + adresse + "`'> " + "<i class='fas fa-folder-open'></i>" + " " + element.name + " </div>\");");
+            }
 
-        	else if(element.isFile()){
-        		
-        		response.write("$('#listeFichiers').append(\"<div class='col-md-2 col-sm-3 pt-4 pb-4 border' id='fichier'> " + "<i class='fas fa-file'></i>" + " " + element.name + " </div>\");");
-        	}
+            else if(element.isFile()){
+                
+                response.write("$('#listeFichiers').append(\"<div class='col-md-2 col-sm-3 pt-4 pb-4 border' id='fichier'> " + "<i class='fas fa-file'></i>" + " " + element.name + " </div>\");");
+            }
 
-        	
+            
 
         }
 
+
         response.write("</script></html>");
         response.end(); 
- 
-    }).listen(PORT);
-});
+
+    });
+
+}).listen(PORT);
